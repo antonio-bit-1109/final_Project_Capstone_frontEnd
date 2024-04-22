5373b62d679d90ea8e0d5498b4b781582dc1621b
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-04-22 23:11:59 +0200

    fix prodotti acquistati

--- src/components/General/Utente/StoricoAcquisti.jsx
+++ src/components/General/Utente/StoricoAcquisti.jsx
@@ -54,9 +54,16 @@ const StoricoAcquisti = () => {
                             {prodottiAcquistati.map((prod, i) => (
                                 <Col key={`hello-${i}`} xs="12" sm="12" md="10" lg="6" xl="6">
                                     <Card
-                                        className="rounded rounded-5 my-4 p-3 position-relative shadow-lg  border border-2 
-                                        effettoVetro text-light scalaAnimazione h-card"
+                                        className=" d-flex  flex-row rounded rounded-5 my-4 p-3 position-relative shadow-lg  border border-2 
+                                        effettoVetro text-light scalaAnimazione gap-4"
                                     >
+                                        <img
+                                            style={{ height: "100px", width: "100px" }}
+                                            className="img-fluid rounded-3 object-fit-cover"
+                                            src={`${LocalHostPath}/img-prodotti/${prod.immagineProdotto}`}
+                                            alt="img prodotto acquistato "
+                                        />
+
                                         <div className="my-1 text-light">
                                             <p className="m-auto">Acquistato il:</p>
                                             <span className="fs-5">
@@ -69,30 +76,21 @@ const StoricoAcquisti = () => {
                                             </span>{" "}
                                         </div>
                                         <div className="d-flex">
-                                            <div className="w-50">
-                                                <p className="display-4">{prod.nomeProdotto}</p>
-                                                <p className="display-6"> Prezzo Singolo : {prod.prezzoProdotto} € </p>
-                                                <p className="display-6"> Unità Acquistate : {prod.quantita}</p>
-                                                <p className="display-6">
+                                            <div>
+                                                <p className="fs-3">{prod.nomeProdotto}</p>
+                                                <p className="fs-3"> Prezzo Singolo : {prod.prezzoProdotto} € </p>
+                                                <p className="fs-3"> Unità Acquistate : {prod.quantita}</p>
+                                                <p className="fs-3">
                                                     {" "}
                                                     Prezzo Totale : {prod.prezzoTotaleTransazione} €
                                                 </p>
                                             </div>
-                                            <div className="w-50 d-flex justify-content-center">
-                                                <img
-                                                    style={{ height: "200px", width: "200px" }}
-                                                    className="img-fluid rounded-3 object-fit-cover"
-                                                    src={`${LocalHostPath}/img-prodotti/${prod.immagineProdotto}`}
-                                                    alt="img prodotto acquistato "
-                                                />
-                                            </div>
                                         </div>
-
                                         {/* <img
-                                                className="img-fluid rounded-3"
-                                                src={`${LocalHostPath}/img-prodotti/${prod.immagineProdotto}`}
-                                                alt=""
-                                            />{" "} */}
+                                            className="img-fluid rounded-3"
+                                            src={`${LocalHostPath}/img-prodotti/${prod.immagineProdotto}`}
+                                            alt=""
+                                        />{" "} */}
                                     </Card>
                                 </Col>
                             ))}
