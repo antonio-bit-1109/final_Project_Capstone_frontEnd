5da9f9664c75e93e8dcfa7791e831fa7216179df
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-05-07 18:54:38 +0200

    fix bugs

--- src/components/BackOffice2/CRUDUtenti/BackOffice2Utenti.jsx
+++ src/components/BackOffice2/CRUDUtenti/BackOffice2Utenti.jsx
@@ -1,5 +1,58 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const BackOffice2Utenti = () => {
-    return <div>BackOffice2Utenti</div>;
+    const navigate = useNavigate();
+    return (
+        <div className="Bg-sfondo-dark altezza-sfondo">
+            <Container>
+                <Row>
+                    <Col xs="12">
+                        {" "}
+                        <div className="h-100 d-flex align-items-center justify-content-center">
+                            <h1 className="text-light display-2 my-3 text-center"> BackOffice </h1>{" "}
+                        </div>
+                    </Col>
+
+                    {/* BOTTONI SCELTA CATEGORIA */}
+                    <Col xs="12">
+                        <div className="d-flex gap-3 mb-3">
+                            <Button
+                                onClick={() => {
+                                    navigate("/BackOffice");
+                                }}
+                                variant="light"
+                                className="rounded-4 text-warning border-warning fw-bold"
+                            >
+                                {" "}
+                                Prodotti{" "}
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    navigate("/BackOffice/Esercizio");
+                                }}
+                                variant="warning "
+                                className="rounded-4 text-light fw-bold"
+                            >
+                                {" "}
+                                Esercizi{" "}
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    navigate("/BackOffice/Utenti");
+                                }}
+                                variant="light"
+                                className="rounded-4 text-warning border-warning fw-bold"
+                            >
+                                {" "}
+                                Utenti
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default BackOffice2Utenti;
---
+++ src/components/BackOffice2/CRUDUtenti/DivMapUtenti.jsx
@@ -0,0 +1,5 @@
+const DivMapUtenti = () => {
+    return <></>;
+};
+
+export default DivMapUtenti;
